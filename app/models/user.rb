dabf6002f3e5fe676c214991969133b2b238ad38
Nikhil Dashore <dashore24@gmail.com>
2020-07-13 16:25:51 +0530

    Added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # validations
+  validates :username, length: { minimum: Constant::MIN_USERNAME_LENGTH },
+                       format: { with: Constant::USERNAME_REGEX },
+                       on: :update, if: :username_changed?
+
+  validates :email, uniqueness: true, presence: true,
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password, presence: true,
+                       length: { minimum: Constant::MIN_PASSWORD_LENGTH }, if: :password_digest_changed?
 end
---
+++ lib/constant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Constant file
+class Constant
+  USERNAME_REGEX = /\A[a-zA-Z0-9\_]+\Z/.freeze
+  MIN_USERNAME_LENGTH = 5
+  MIN_PASSWORD_LENGTH = 8
+end
